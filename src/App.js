939bed82ccfad4da6274f9b290a05f77d2f5d622
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-02-15 11:44:19 -0500

    react project set up, page skeleton done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import Error from './pages/Error';
+import SingleRoom from './pages/SingleRoom';
+import Navbar from './components/Navbar';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms' component={Rooms} />
+        <Route exact path='/rooms/:roomname' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Rooms.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rooms = () => {
+  return <>Rooms</>;
+};
+
+export default Rooms;
