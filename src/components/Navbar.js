c3db59c1d49ef120c705450151b903bf88e283f3
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-02-15 12:08:52 -0500

    responsive nav bar comnplete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import logo from '../images/logo.svg';
 
 const Navbar = () => {
-  return <>Navbar</>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src={logo} alt='Beach resort' />
+          </Link>
+          <button type='button' className='nav-btn'>
+            <FaAlignRight className='nav-icon' onClick={handleToggle} />
+          </button>
+        </div>
+        <ul className={isOpen ? 'nav-links show-nav' : 'nav-links'}>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/rooms'>Rooms</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
